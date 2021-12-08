25d66be8231833c374fb25db51d844cce71f5c72
SeintApril <seintaprilzaw1996@gmail..com>
2021-12-09 04:18:54 +0700

    link each button and sound properly

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        let title = sender.currentTitle!
+        playSound(title: title)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            // Bring's sender's opacity back up to fully opaque
+            sender.alpha = 1.0
+          }
+    }
+    
+    func playSound(title : String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
 }
 
